3f27fb3ac0737f7ac4d0258bdc786dbaafea6c08
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2026-09-06 19:07:10 +0530

    producer done test

--- Order_Producer/src/main/java/org/visweshvar/kafka/OrderProducer.java
+++ Order_Producer/src/main/java/org/visweshvar/kafka/OrderProducer.java
@@ -1,12 +1,26 @@
 package org.visweshvar.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 import java.util.Properties;
 
 public class OrderProducer {
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.setProperty("bootstrap.server","localhost:9092");
+        props.setProperty("bootstrap.servers","localhost:9092");
         props.setProperty("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
         props.setProperty("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, Integer> producer = new KafkaProducer<>(props);
+        ProducerRecord<String, Integer> record = new ProducerRecord<>("OrderTopic", "TUF A14 gaming laptop" ,10);
+        try {
+            producer.send(record);
+            System.out.println("Message sent successfully");
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            producer.close();
+        }
     }
 }
