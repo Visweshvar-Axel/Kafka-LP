c0bd3d124019eedca7bfa873e04ca5b9b0e59acf
VISWESHVAR R <118894317+Visweshvar-Axel@users.noreply.github.com>
2026-09-12 18:17:10 +0530

    Update OrderConsumer.java
    Co-Authored-By: Visweshvar <182783895+visweshvar-ust@users.noreply.github.com>

--- Order_Consumer/src/main/java/org/visweshvar/kafka/OrderConsumer.java
+++ Order_Consumer/src/main/java/org/visweshvar/kafka/OrderConsumer.java
@@ -13,8 +13,8 @@ public class OrderConsumer {
     public static void main(String[] args) {
         Properties props = new Properties();
         props.setProperty("bootstrap.servers","localhost:9092");
-        props.setProperty("key.Deserializer","org.apache.kafka.common.serialization.StringDeserializer");
-        props.setProperty("value1.Deserializer","org.apache.kafka.common.serialization.IntegerDeserializer");
+        props.setProperty("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer","org.apache.kafka.common.serialization.IntegerDeserializer");
         props.setProperty("group.id","OrderGroup");
 
         KafkaConsumer<String,Integer> consumer = new KafkaConsumer<>(props);
